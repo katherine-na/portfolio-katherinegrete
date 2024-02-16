c6c0ada9782d08efe86e85c53d0e67a83ec39e50
Katherine Negrete <katherine@vinneren.com.mx>
2024-02-16 00:26:36 -0600

    update cv

--- src/components/Biography/Biography.jsx
+++ src/components/Biography/Biography.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './Biography.css'
-import MyCV from "../../assets/CV/Front-End-Dev_Katherine_Negrete.pdf";
+import MyCV from "../../assets/CV/Front-End-Dev_Katherine_Negrete";
 
 const Biography = () => {
 
